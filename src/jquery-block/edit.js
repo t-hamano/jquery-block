694ca579bc6757a4e799821cb74c8d2b412b7902
Aki Hamano <tetsuaki.hamano@gmail.com>
2025-07-26 22:22:20 +0900

    Format

--- src/jquery-block/edit.js
+++ src/jquery-block/edit.js
@@ -1,22 +1,22 @@
-import $ from "jquery";
-import "select2";
-import "jquery-validation";
+import $ from 'jquery';
+import 'select2';
+import 'jquery-validation';
 import { useBlockProps } from '@wordpress/block-editor';
 import { useRefEffect } from '@wordpress/compose';
 
 export default function Edit() {
-	const select2Ref = useRefEffect( (element) => {
-			$( element ).select2( {
-				dropdownParent: $( element ).parent(),
-				width: '100%',
-			} );
+	const select2Ref = useRefEffect( ( element ) => {
+		$( element ).select2( {
+			dropdownParent: $( element ).parent(),
+			width: '100%',
+		} );
 
-			return () => {
-				$( element ).select2( 'destroy' );
-			};
+		return () => {
+			$( element ).select2( 'destroy' );
+		};
 	}, [] );
 
-	const validationFormRef = useRefEffect( (element) => {
+	const validationFormRef = useRefEffect( ( element ) => {
 		$( element ).validate( {
 			rules: {
 				username: { required: true, minlength: 5 },
@@ -37,10 +37,13 @@ export default function Edit() {
 				<option value="cherry">Cherry</option>
 			</select>
 			<h2>jQuery Validation</h2>
-			<form ref={ validationFormRef } onSubmit={ ( e ) => {
-				e.preventDefault();
-				window.alert( 'Form submitted' );
-			} }>
+			<form
+				ref={ validationFormRef }
+				onSubmit={ ( e ) => {
+					e.preventDefault();
+					window.alert( 'Form submitted' );
+				} }
+			>
 				<input type="text" name="username" autoComplete="off" />
 				<button type="submit">Submit</button>
 			</form>
